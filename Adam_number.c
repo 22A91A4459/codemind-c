44489862fedd0f28283ee59f4e22a2559f92663b
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-01-23 23:33:03 +0530

    #include<stdio.h> int main() {     int n,d,i,k,rev=0,revk=0,num;     scanf("%d",&n);     k=n*n;     num=n;     while(n)     {         d=n%10;         rev=rev*10+d;         n/=10;     }     i=rev*rev;     while(k)     {         d=k%10;         revk=revk*10+d;         k/=10;     }     if(num%10)     {     if(revk==i)     printf("True");     else     printf("False");     }     else     printf("False"); }

---
+++ Adam_number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,i,k,rev=0,revk=0,num;
+    scanf("%d",&n);
+    k=n*n;
+    num=n;
+    while(n)
+    {
+        d=n%10;
+        rev=rev*10+d;
+        n/=10;
+    }
+    i=rev*rev;
+    while(k)
+    {
+        d=k%10;
+        revk=revk*10+d;
+        k/=10;
+    }
+    if(num%10)
+    {
+    if(revk==i)
+    printf("True");
+    else
+    printf("False");
+    }
+    else
+    printf("False");
+}
