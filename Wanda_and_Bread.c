59033591f81eeee62a736e642af8c7b8e47f3204
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-05-20 00:39:50 +0530

    #include<stdio.h> int main() {     int n,m,k;     scanf("%d%d%d",&n,&m,&k);     int x;     x=m*k;     if(n<=x)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,k;
+    scanf("%d%d%d",&n,&m,&k);
+    int x;
+    x=m*k;
+    if(n<=x)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
