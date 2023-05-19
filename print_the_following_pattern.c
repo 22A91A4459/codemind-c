bc43a20e4fc9ba9b0ec1dce6d317cb8af1d6dfbd
Navya kanneti <121533303+22A91A4459@users.noreply.github.com>
2023-05-20 00:06:00 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
         for(j=1;j<=i;j++)
         {
-            printf("%c ",'A'-1+i);
+            printf("%d",j);
         }
         printf("
 ");
     }
-    return 0;
 }
